30dcf3574573b7421227cd1c40210ab2053a3cca
Chip Bell <chip.bell@spinen.com>
2013-08-31 11:31:58 -0400

    Added some comments

--- js/app.js
+++ js/app.js
@@ -1,5 +1,12 @@
+
+/*
+ * Setup the application, by creating a router and initializing it.
+ * The router will then look at the url and route accordingly (it
+ * should go to the index). We also turn on history tracking so our
+ * back button works.
+ */
 var router = null;
 $(document).ready(function() {
-	router = new FarmersMarketRouter(); 
+	router = new FarmersMarketRouter();
 	Backbone.history.start({pushState:false});
 });
--- js/routes.js
+++ js/routes.js
@@ -1,4 +1,8 @@
 var FarmersMarketRouter = Backbone.Router.extend({
+	/*
+	 * Here is a list of all the routes for our app:
+	 * the pattern is '/url/name/:variable1' : 'functionNameToCall'
+	 */
 	routes: {
 		'': 'index',
 		'search/:lat,:lng': 'searchByLatLng',
@@ -6,40 +10,73 @@ var FarmersMarketRouter = Backbone.Router.extend({
 		'details/:market': 'marketDetails',
 	},
 
+	/*
+	 * Renders the index route, with all of the search information
+	 */
 	index: function() {
 		var view = new SearchView();
 		view.render();
 	},
 
+	/*
+	 * Callback for a lat/long search
+	 */
 	searchByLatLng: function(lat, lng) {
 		this.collection = new SearchResultsCollection();
 		this.collection.setLatLng(lat, lng);
+
+		// specify the callback for when the ajax call completes
 		this.listenToOnce(this.collection, 'sync', this.fetchFinish);
 		this.collection.fetch();
 	},
 
+	/*
+	 * Callback for a zip search
+	 */
 	searchByZip: function(zip) {
 		this.collection = new SearchResultsCollection();
 		this.collection.setZip(zip);
+
+		// specify the callback for when the ajax call completes
 		this.listenToOnce(this.collection, 'sync', this.fetchFinish);
+
 		this.collection.fetch();
-		console.log("FETCHING");
 	},
 
+	/*
+	 * Searches use AJAX, which is asynchronous. This is the callback
+	 * for when a search completes. It essentially renders the search results
+	 * to the page
+	 */
 	fetchFinish: function() {
 		var view = new ListView({'collection':this.collection});
 		view.render();
 	},
 
+	/*
+	 * Renders the details for a market based on its ID.
+	 */
 	marketDetails: function(id) {
 		this.model = new MarketDetails();
 		this.model.set('id', id);
+
+		/*
+		 * the detail results for a market does not include the market name,
+		 * so we provide that to the details as well, so we can use that when
+		 * displaying the market
+		 */
 		this.model.set('marketname', this.collection.get(id).get('marketname'));
 
+		/* once we've ajax-pulled the details for this market, we display them
+		 * by calling our detailsRetrieved callback
+		 */
 		this.listenToOnce(this.model, 'sync', this.detailsRetrieved);
 		this.model.fetch();
 	},
 
+	/*
+	 * The callback for when details are retrieved for a farmer's market
+	 */
 	detailsRetrieved: function() {
 		var view = new DetailsView({ 'model': this.model});
 		view.render();
